97af4060d7f971966c79c143d6834d5d1e677780
Martin Onufrak <martinonufrak@outlook.sk>
2022-12-02 03:43:56 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,23 @@
+import java.io.File
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun parseInput(input: String):List<List<Int>> = input.split("\n\n").map { elf:String -> elf.lines().map { it.toInt() }}
+
+    fun part1(input: String): Int {
+        val data:List<List<Int>> = parseInput(input)
+        return data.maxOf { it.sum() }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: String): Int {
+        val data:List<List<Int>> = parseInput(input)
+        return data.map { it.sum() }.sortedDescending().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput:String = File("src/Day01_test.txt").readText()
+    check(part1(testInput) == 24000)
 
-    val input = readInput("Day01")
+    val input:String = File("src/Day01.txt").readText()
     println(part1(input))
     println(part2(input))
 }
